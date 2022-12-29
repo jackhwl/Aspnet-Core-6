192ce3b72630598da422f993c30a95429d051cc4
Wenlin Huang <whuang@viglobal.com>
2022-12-28 21:42:10 -0500

    Demo: Creating View Components

--- Core6-Fundamentals/BethanysPieShop/Controllers/PieController.cs
+++ Core6-Fundamentals/BethanysPieShop/Controllers/PieController.cs
@@ -15,12 +15,31 @@ namespace BethanysPieShop.Controllers
 			_categoryRepository = categoryRepository;
 		}
 
-		public IActionResult List() 
+		//public IActionResult List() 
+		//{
+		//	//ViewBag.CurrentCategory = "Cheese cakes";
+		//	//return View(_pieRepository.AllPies);
+		//	var piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "All Pies");
+		//	return View(piesListViewModel);
+		//}
+
+		public ViewResult List(string category)
 		{
-			//ViewBag.CurrentCategory = "Cheese cakes";
-			//return View(_pieRepository.AllPies);
-			var piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "All Pies");
-			return View(piesListViewModel);
+			IEnumerable<Pie> pies;
+			string? currentCategory;
+
+			if(string.IsNullOrEmpty(category))
+			{
+				pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+				currentCategory = "All pies";
+			}
+			else
+			{
+				pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName== category).OrderBy(p => p.PieId);
+				currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+			}
+
+			return View(new PieListViewModel(pies, currentCategory));
 		}
 
 		public IActionResult Details(int id)
