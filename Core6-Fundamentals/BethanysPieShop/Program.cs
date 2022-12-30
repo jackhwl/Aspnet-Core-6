427b22e9626434b475082065fbdefa83b5527342
Wenlin Huang <whuang@viglobal.com>
2022-12-30 16:12:26 -0500

    Demo: Recreating the Form Using Razor Pages

--- Core6-Fundamentals/BethanysPieShop/Program.cs
+++ Core6-Fundamentals/BethanysPieShop/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
@@ -31,6 +32,7 @@ app.MapDefaultControllerRoute();
 //app.MapControllerRoute(
 //	name: "default",
 //	pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 DbInitializer.Seed(app);
 app.Run();
