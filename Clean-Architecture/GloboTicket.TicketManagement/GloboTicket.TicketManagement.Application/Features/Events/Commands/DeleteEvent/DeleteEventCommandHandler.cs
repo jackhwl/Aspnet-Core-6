dde9968b282bbca69bdf3ad57f523fd7da0a7949
Wenlin Huang <whuang@viglobal.com>
2023-01-01 16:48:47 -0500

    Demo: Using Commands to Create a New Entity

---
+++ Clean-Architecture/GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.DeleteEvent
+{
+    internal class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IAsyncRepository<Event> _eventRepository;
+
+        public DeleteEventCommandHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventToDelete = await _eventRepository.GetByIdAsync(request.EventId);
+            await _eventRepository.DeleteAsync(eventToDelete);
+            return Unit.Value;
+        }
+    }
+}
--- Clean-Architecture/GloboTicket.TicketManagement/ReadMe.md
+++ Clean-Architecture/GloboTicket.TicketManagement/ReadMe.md
@@ -6,3 +6,4 @@
 * Demo: Writing the Application Logic in the Request Handler
 * Demo: Adding CQRS into the Application Architecture
 * Demo: Organizing the Code Using Feature
+* Demo: Using Commands to Create a New Entity
