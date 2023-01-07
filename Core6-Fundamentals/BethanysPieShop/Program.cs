40097b7b9a3b9848d243376452e0da5db6470ec7
Wenlin Huang <whuang@viglobal.com>
2023-01-07 10:54:33 -0500

    Demo: Adding Authentication

--- Core6-Fundamentals/BethanysPieShop/Program.cs
+++ Core6-Fundamentals/BethanysPieShop/Program.cs
@@ -1,8 +1,11 @@
 using BethanysPieShop.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BethanysPieShopDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BethanysPieShopDbContextConnection' not found.");
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
@@ -20,8 +23,11 @@ builder.Services.AddControllersWithViews()
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
-	options.UseSqlServer(builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+	options.UseSqlServer(connectionString); //builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>() //options => options.SignIn.RequireConfirmedAccount = true
+    .AddEntityFrameworkStores<BethanysPieShopDbContext>();
 
 builder.Services.AddServerSideBlazor();
 
