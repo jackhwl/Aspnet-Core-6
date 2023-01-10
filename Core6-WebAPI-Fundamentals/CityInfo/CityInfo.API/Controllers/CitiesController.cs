d193aef0d3c6118cfee209793829f9d5467b76e4
Wenlin Huang <jackhwl@hotmail.com>
2023-01-09 20:06:45 -0500

    Demo: Improving the Architecture with Model Classes

--- Core6-WebAPI-Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ Core6-WebAPI-Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -10,11 +10,12 @@ namespace CityInfo.API.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>
-            {
-                new {id = 1, Name = "New Yourk City"},
-                new {id = 2, Name = "Antwerp"}
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id) 
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
 }
