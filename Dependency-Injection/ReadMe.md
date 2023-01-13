c51af2c03d434af71a8d5c4817f320956023f0d0
Wenlin Huang <jackhwl@hotmail.com>
2023-01-12 19:49:18 -0500

    Why Use Dependency Injection?

--- Dependency-Injection/ReadMe.md
+++ Dependency-Injection/ReadMe.md
@@ -1,1 +1,2 @@
 ## Section 2: Registering and Injecting Services
+* Why Use Dependency Injection?
---
+++ Dependency-Injection/src/TennisBookings.Shared/Weather/AmazingWeatherForecaster.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisBookings.Shared.Weather
+{
+	public class AmazingWeatherForecaster
+	{
+		public Task<WeatherResult> GetCurrentWeatherAsync(string city)
+		{
+			return Task.FromResult(new WeatherResult()
+			{
+				City= city,
+				Weather = new WeatherCondition
+				{
+					Summary = "Sun"
+				}
+			});
+		}
+	}
+}
