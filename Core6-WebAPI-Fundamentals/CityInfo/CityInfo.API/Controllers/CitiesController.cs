e12157c99c0e1c632acc23a858efce5b928f6f24
Wenlin Huang <jackhwl@hotmail.com>
2023-01-26 10:05:16 -0500

    Demo: Returning Data from the Repository When Requesting Resources Part2

--- Core6-WebAPI-Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ Core6-WebAPI-Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using CityInfo.API.Models;
+﻿using AutoMapper;
+using CityInfo.API.Models;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,27 +11,20 @@ namespace CityInfo.API.Controllers
     public class CitiesController : ControllerBase
     {
         private readonly ICityInfoRepository _cityInfoRepository;
-        public CitiesController(ICityInfoRepository cityInfoRepository) 
+        private readonly IMapper _mapper;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository, IMapper mapper) 
         {
-            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository)); 
+            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
             var cityEntities = await _cityInfoRepository.GetCitiesAsync();
-            var results = new List<CityWithoutPointsOfInterestDto>();
-            foreach(var cityEntity in cityEntities)
-            {
-                results.Add(new CityWithoutPointsOfInterestDto
-                {
-                    Id= cityEntity.Id,
-                    Description= cityEntity.Description,
-                    Name= cityEntity.Name
-                });
-            }
 
-            return Ok(results);
+            return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
         }
 
         [HttpGet("{id}")]
