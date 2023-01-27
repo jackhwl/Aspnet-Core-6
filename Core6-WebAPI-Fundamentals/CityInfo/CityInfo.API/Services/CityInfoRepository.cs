f0ee9b6ec6a636f0c617c3f57c07a4331445434a
Wenlin Huang <jackhwl@hotmail.com>
2023-01-27 11:18:33 -0500

    Demo: Creating a Token

--- Core6-WebAPI-Fundamentals/CityInfo/CityInfo.API/Services/CityInfoRepository.cs
+++ Core6-WebAPI-Fundamentals/CityInfo/CityInfo.API/Services/CityInfoRepository.cs
@@ -18,7 +18,7 @@ namespace CityInfo.API.Services
             return await _context.Cities.OrderBy(c => c.Name).ToListAsync();
         }
 
-        public async Task<IEnumerable<City>> GetCitiesAsync(string? name, string? searchQuery, int pageNumber, int pageSize)
+        public async Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(string? name, string? searchQuery, int pageNumber, int pageSize)
         {
             var collection = _context.Cities as IQueryable<City>;
 
@@ -35,11 +35,17 @@ namespace CityInfo.API.Services
                     .Where(a => a.Name.Contains(searchQuery) || (a.Description != null && a.Description.Contains(searchQuery)));
             }
 
-            return await collection
+            var totalItemCount = await collection.CountAsync();
+
+            var paginationMetadata = new PaginationMetadata(totalItemCount, pageSize, pageNumber);
+
+            var collectionToReturn = await collection
                 .OrderBy(c => c.Name)
                 .Skip(pageSize * (pageNumber - 1))
                 .Take(pageSize)
                 .ToListAsync();
+
+            return (collectionToReturn, paginationMetadata);
         }
 
         public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
