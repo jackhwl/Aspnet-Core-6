3b4d108b0a80b6254c4a3599744863f352479cd3
Wenlin Huang <jackhwl@hotmail.com>
2023-03-08 20:08:27 -0500

    Logging out of the Identity Provider

--- SecuringAspNetCore6WithOAuth2AndOIDC/README.md
+++ SecuringAspNetCore6WithOAuth2AndOIDC/README.md
@@ -12,3 +12,4 @@
 * Demo - Configuring IdentityServer to Log in with The Authorization Code Flow
 * Demo - Logging in with the Authorization Code Flow
 * Demo - Logging out of Our Web Application
+* Logging out of the Identity Provider
--- SecuringAspNetCore6WithOAuth2AndOIDC/Starter_files/ImageGallery.Client/Controllers/AuthenticationController.cs
+++ SecuringAspNetCore6WithOAuth2AndOIDC/Starter_files/ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,9 @@ public class AuthenticationController : Controller
     {
         // clears the local cookie
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        // Redirects to the IDP linked to scheme "OpenIdConnectDefaults.AuthenticationScheme" (oidc)
+        // so it can clear its own session/cookie
+        await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
     }
 }
