6fcd1a0251ef9a2b17fcb92af7b8ce8e148e63fc
Wenlin Huang <jackhwl@hotmail.com>
2023-05-06 16:28:44 -0400

    Demo - Asserting on Strings

---
+++ Core6-WebAPI-UnitTesting/EmployeeManagement.Test/EmployeeTests.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test;
+public class EmployeeTests
+{
+    [Fact]
+    public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameIsConcatenation()
+    {
+        var employee = new InternalEmployee("Kevin", "Dockx", 0, 2500, false, 1);
+
+        employee.FirstName = "Lucia";
+        employee.LastName = "Shelton";
+
+        Assert.Equal("Lucia Shelton", employee.FullName);
+    }
+}
