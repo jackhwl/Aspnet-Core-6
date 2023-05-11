18570084827e482d3df9a59610be057a49770290
Wenlin Huang <jackhwl@hotmail.com>
2023-05-10 20:43:21 -0400

    Demo - Creating and Using a Mock Object

---
+++ Core6-WebAPI-UnitTesting/EmployeeManagement.Test/MoqTests.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.Services.Test;
+using Moq;
+
+namespace EmployeeManagement.Test;
+public class MoqTests
+{
+    [Fact]
+    public void FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated()
+    {
+        var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+        //var employeeFactory = new EmployeeFactory();
+        var employeeFactoryMock = new Mock<EmployeeFactory>();
+        var employeeService = new EmployeeService(employeeManagementTestDataRepository, employeeFactoryMock.Object);
+
+        var employee = employeeService.FetchInternalEmployee(Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
+
+        Assert.Equal(400, employee.SuggestedBonus);
+    }
+
+    [Fact]
+    public void CreateInternalEmployee_InternalEmployeeCreated_SuggestedBonusMustBeCalculated()
+    {
+        // Arrange
+        var employeeManagementTestDataRepository =
+          new EmployeeManagementTestDataRepository();
+        var employeeFactoryMock = new Mock<EmployeeFactory>();
+        var employeeService = new EmployeeService(employeeManagementTestDataRepository, employeeFactoryMock.Object);
+
+        decimal suggestedBonus = 200;
+        var employee = employeeService.CreateInternalEmployee("Kevin", "Dockx");
+
+        Assert.Equal(suggestedBonus, employee.SuggestedBonus);
+    }
+}
