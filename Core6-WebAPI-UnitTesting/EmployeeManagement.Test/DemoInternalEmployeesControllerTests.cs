d3ca7421f41d2d02a2c2cda0372b30fc6ae84152
Wenlin Huang <jackhwl@hotmail.com>
2023-05-12 11:38:59 -0400

    Demo - Testing Validation and ModelState

---
+++ Core6-WebAPI-UnitTesting/EmployeeManagement.Test/DemoInternalEmployeesControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using EmployeeManagement.Business;
+using EmployeeManagement.Controllers;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EmployeeManagement.Test;
+public class DemoInternalEmployeesControllerTests
+{
+    [Fact]
+    public async Task CreateInternalEmployee_InvalidInput_MustReturnBadRequest()
+    {
+        var employeeServiceMock = new Mock<IEmployeeService>();
+        var mapperMock = new Mock<IMapper>();
+        var demoInternalEmployeesController = new DemoInternalEmployeesController(employeeServiceMock.Object, mapperMock.Object);
+
+        var internalEmployeeForCreationDto = new InternalEmployeeForCreationDto();
+        //{
+        //    FirstName = string.Empty, LastName = "Smith"
+        //};
+
+        demoInternalEmployeesController.ModelState.AddModelError("FirstName", "Required");
+
+        var result = await demoInternalEmployeesController.CreateInternalEmployee(internalEmployeeForCreationDto);
+
+        var actionResult = Assert.IsType<ActionResult<Models.InternalEmployeeDto>>(result);
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+
+        Assert.IsType<SerializableError>(badRequestResult.Value);
+    }
+}
