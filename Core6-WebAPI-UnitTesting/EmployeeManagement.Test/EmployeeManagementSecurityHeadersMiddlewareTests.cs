91ca7d9a1af35d5708610afdf9769a56e56f0e0d
Wenlin Huang <jackhwl@hotmail.com>
2023-05-14 13:10:23 -0400

    Demo - Unit Testing Middleware

---
+++ Core6-WebAPI-UnitTesting/EmployeeManagement.Test/EmployeeManagementSecurityHeadersMiddlewareTests.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Middleware;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeManagement.Test;
+public class EmployeeManagementSecurityHeadersMiddlewareTests
+{
+    [Fact]
+    public async Task InvokeAsync_Invoke_SetsExpectedResponseHeaders()
+    {
+        var httpContext = new DefaultHttpContext();
+        RequestDelegate next = (HttpContext httpContext) => Task.CompletedTask;
+
+        var middleware = new EmployeeManagementSecurityHeadersMiddleware(next);
+
+        await middleware.InvokeAsync(httpContext);
+
+        var cspHeader = httpContext.Response.Headers["Content-Security-Policy"].ToString();
+        var xContextTypeOptionsHeader = httpContext.Response.Headers["X-Content-Type-Options"].ToString();
+
+        Assert.Equal("default-src 'self';frame-ancestors 'none';", cspHeader);
+        Assert.Equal("nosniff", xContextTypeOptionsHeader);
+    }
+}
